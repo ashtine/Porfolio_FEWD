50310ed6241508a3b2636df10b958f314d08a914
ashtine <ashtinet@yahoo.com>
2017-03-16 20:16:38 -0400

    Fixing from big mistake
    added in about.html

--- javascript/work.js
+++ javascript/work.js
@@ -1,1 +1,33 @@
-work.js
+console.log("Hello World from main.js!");
+
+var allContainers = document.querySelectorAll(".work-container");
+
+// function populateImages() {
+//  for (var i = allContainers.length - 1; i >= 0; i--) {
+//          allContainers[i].style.display = "inherit";
+//      }   
+// }
+
+function populatePage() {
+    TweenMax.staggerFromTo(".work-container", 3, 
+    {
+    display: "none",
+    opacity: 0,
+    ease:Elastic.easeOut,
+    },
+    {
+        display: "inherit",
+        opacity: 1, ease:Elastic.easeOut,
+    }, 1
+    );
+}
+
+populatePage();
+
+
+// TweenMax.command(".class-name", 
+//          #,  
+//          {css: "style2", css: "style1"}
+// );
+
+// # is the duration in seconds
